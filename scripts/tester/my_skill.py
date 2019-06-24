78ad4c9045f4463b0a38e97645b6113cb68af15e
Ieuan Jones <ieuanj28@googlemail.com>
2019-06-24 16:54:51 +0100

    clean up my_skill.py; reconfigure .gitignore

---
+++ scripts/tester/my_skill.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+from mycroft_ros.msg import *
+from mycroft_ros.srv import *
+from std_msgs.msg import String
+import actionlib
+from mycroft_ros.helpers import *
+
+path = "/home/vagrant/dev/catkin_ws/src/mycroft_ros/scripts/tester"
+
+def mycroft_ros_callback(message):
+    print('hello')
+
+def main():
+    rospy.init_node('mycroft_skill_test')
+    rospy.loginfo(rospy.get_caller_id() + " started")
+    manager = IntentManager().register_callback('testerMycroftRos', mycroft_ros_callback)
+    rospy.Subscriber('mycroft/tester', IntentResponse, manager.handle_intent)
+    my_intent = IntentBuilder("MycroftRos").require("mycroft").require("ros").build()
+    initialised = SkillBuilder(path).intent(my_intent).intent_file("mytest.intent") \
+    .intent_file("random.lol.intent").entity("thing.entity").buildAndRegister()
+    if initialised:
+       print("created")
+    else:
+       print("not created")
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
